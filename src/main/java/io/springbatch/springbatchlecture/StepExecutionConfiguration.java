a0462272fae80563af773688f95d8bebbd2cd8ee
김경훈 <sisipapa@gmail.com>
2022-12-19 00:20:07 +0900

    StepExecution

--- src/main/java/io/springbatch/springbatchlecture/StepExecutionConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/StepExecutionConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class StepConfiguration {
+public class StepExecutionConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -24,13 +24,20 @@ public class StepConfiguration {
         return jobBuilderFactory.get("Job")
                 .start(step1())
                 .next(step2())
+                .next(step3())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new CustomTasklet())
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step1 execute complete");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
                 .build();
     }
 
@@ -41,6 +48,18 @@ public class StepConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("step2 execute complete");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step3 execute complete");
                         return RepeatStatus.FINISHED;
                     }
                 })
