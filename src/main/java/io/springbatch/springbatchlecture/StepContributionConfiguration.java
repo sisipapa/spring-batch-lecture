82b2a9c601cbfeca03be2ac0393ccd74d192384b
김경훈 <sisipapa@gmail.com>
2022-12-20 23:27:35 +0900

    StepContribution

--- src/main/java/io/springbatch/springbatchlecture/StepContributionConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/StepContributionConfiguration.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class StepExecutionConfiguration {
+public class StepContributionConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
