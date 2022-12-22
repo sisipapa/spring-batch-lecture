8246d30f3dfadc6a91febc6762cfd1cb11877aa7
김경훈 <sisipapa@gmail.com>
2022-12-23 00:08:19 +0900

    JobRepository

--- src/main/java/io/springbatch/springbatchlecture/JobRepositoryConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobRepositoryConfiguration.java
@@ -2,6 +2,7 @@ package io.springbatch.springbatchlecture;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -14,50 +15,42 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class ExecutionContextConfiguration {
+public class JobRepositoryConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
-
-    private final ExecutionContextTasklet1 executionContextTasklet1;
-    private final ExecutionContextTasklet2 executionContextTasklet2;
-    private final ExecutionContextTasklet3 executionContextTasklet3;
-    private final ExecutionContextTasklet4 executionContextTasklet4;
+    private final JobExecutionListener jobExecutionListener;
 
     @Bean
-    public Job job(){
+    public Job BatchJob(){
         return jobBuilderFactory.get("Job")
                 .start(step1())
                 .next(step2())
-                .next(step3())
-                .next(step4())
+                .listener(jobExecutionListener)
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(executionContextTasklet1)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        return RepeatStatus.FINISHED;
+                    }
+                })
                 .build();
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(executionContextTasklet2)
-                .build();
-    }
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet(executionContextTasklet3)
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet(executionContextTasklet4)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        return RepeatStatus.FINISHED;
+                    }
+                })
                 .build();
     }
 }
