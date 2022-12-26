ebea6c180f03c7aceef8836d3fd2a4954badb9f5
김경훈 <sisipapa@gmail.com>
2022-12-26 11:47:32 +0900

    StepNext

--- src/main/java/io/springbatch/springbatchlecture/StartNextConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/StartNextConfiguration.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -16,7 +14,7 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class SimpleJobConfiguration {
+public class StartNextConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -27,37 +25,15 @@ public class SimpleJobConfiguration {
                 .start(step1())
                 .next(step2())
                 .next(step3())
-                .incrementer(new RunIdIncrementer())
-                .preventRestart()
-                .validator(new JobParametersValidator() {
-                    @Override
-                    public void validate(JobParameters jobParameters) throws JobParametersInvalidException {
-
-                    }
-                })
-                .listener(new JobExecutionListener() {
-                    @Override
-                    public void beforeJob(JobExecution jobExecution) {
-
-                    }
-
-                    @Override
-                    public void afterJob(JobExecution jobExecution) {
-
-                    }
-                })
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step1 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step1 has executed");
+                    return RepeatStatus.FINISHED;
                 })
                 .build();
     }
@@ -81,8 +57,8 @@ public class SimpleJobConfiguration {
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                         chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-                        stepContribution.setExitStatus(ExitStatus.STOPPED);
+//                        chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
+//                        stepContribution.setExitStatus(ExitStatus.STOPPED);
                         System.out.println("step3 has executed");
                         return RepeatStatus.FINISHED;
                     }
